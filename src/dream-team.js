b770efbd9255561c0033b24774d6de5eca750d0f
Olya Y <f_akora@mail.ru>
2022-10-15 00:08:13 +0300

    feat: finished dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+  if (Array.isArray(arr)==false) return false; 
+  let result = [];
+  for (let i = 0; i < arr.length; i++){
+    if (typeof arr[i] != "string"||arr[i]==="") {
+      continue;
+    } else {
+      arr[i]=arr[i].trimStart();
+      result.push(arr[i][0].toUpperCase());
+    }
+  }
+  final = result.sort().join("");  
+  return final;
 }
 
 module.exports = {
